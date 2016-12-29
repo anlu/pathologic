4d79aeb73d1902244d90555e52c816c53493d596
anlu <anlu@getbetter.co>
2016-12-28 23:17:56 -0800

    Some boxes

--- pathologic.js
+++ pathologic.js
@@ -1,12 +1,24 @@
-var ctx = document.getElementById('pathologic').getContext('2d');
+let ctx = document.getElementById('pathologic').getContext('2d');
+
+let board = [
+  ['0', 'X', '0'],
+  ['X', 'X', 'X'],
+  ['0', 'X', '0'],
+];
 
 function draw(timestamp) {
   ctx.globalCompositeOperation = 'destination-over';
+  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 
-  console.log(ctx.canvas.width);
-  console.log(ctx.canvas.height);
+  for (let row in board) {
+    for (let col in board) {
+      ctx.fillRect(0, 0, 32, 32);
+      ctx.translate(34, 0);
+    }
+    ctx.translate(-(board[0].length * 34), 34);
+  }
 
-  ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+  ctx.setTransform(1, 0, 0, 1, 0, 0);
 }
 
 window.requestAnimationFrame(draw);
